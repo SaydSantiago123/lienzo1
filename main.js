5a3e30b1f9294677fe16c3c436a9dee976cb5ee3
SaydSantiago123 <98667091+SaydSantiago123@users.noreply.github.com>
2023-04-25 17:25:41 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function setup(){canvas=createCanvas(280,280);
+    canvas.center();
+    background("red");canvas.mouseReleased(classifyCanvas);synth=window.speechSynthesis;
+}
+
+function preload(){
+classifier=ml5.imageClassifier("DoodleNet")
+}
+
+function draw() {
+
+    // Establece stroke weight en 13
+    strokeWeight(13);
+    // Establece stroke color a black
+    stroke(0);
+    // Si es mouse está presionado, dibuja una línea entre la posición anterior y actual del mouse
+    if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+  }
+function classifyCanvas(){
+classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+      }
+      console.log(results);
+      document.getElementById('label').innerHTML = 'Etiqueta: ' + results[0].label;
+    
+      document.getElementById('confidence').innerHTML = 'Confianza: ' + Math.round(results[0].confidence * 100) + '%';
+    
+      utterThis = new SpeechSynthesisUtterance(results[0].label);
+      synth.speak(utterThis);
+}
